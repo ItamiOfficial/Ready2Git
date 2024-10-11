da0cb4dcf7e3921848b5635dfc7769c914dc2a47
fdai7355 <janis-daniel.rohrbach@informatik.hs-fulda.de>
2024-10-11 11:22:19 +0200

    Vierter Commit, Warum muss ich mich erst anmelden?!??!?!

--- Programm.java
+++ Programm.java
@@ -1,5 +1,5 @@
 public class Programm {
     public static void main(String[] args) {
-        System.out.println("Hallo, ich bin ein Programm");
+        System.out.println("Hallo, ich bin ein Programm. Und ich bin in einem Git Repository");
     }
 }
